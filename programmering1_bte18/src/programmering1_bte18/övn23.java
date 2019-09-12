7ab5af44e9173d9093409c56a93ac679a7ef34d1
daande01 <daniel.evert.andersson@gmail.com>
2019-09-12 14:09:42 +0200

    new file

--- programmering1_bte18/src/programmering1_bte18/övn23.java
+++ programmering1_bte18/src/programmering1_bte18/övn23.java
@@ -1,5 +1,22 @@
 package programmering1_bte18;
 
+import javax.swing.JOptionPane;
+
 public class övn23 {
+
+	public static void main(String[] args) {
+
+		String ålderString=JOptionPane.showInputDialog("ange din ålder");
+
+
+		int age = Integer.parseInt(ålderString);
+
+		//jobb kvar
+
+		JOptionPane.showMessageDialog(null, "du fyller " +jämt+"om" +kvar);
+
+
+	}
+
 
 }
