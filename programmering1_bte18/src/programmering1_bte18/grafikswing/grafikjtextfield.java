984b661013a5408532dbe43ae845dae5a94ae8f6
daande01 <daniel.evert.andersson@gmail.com>
2020-01-15 15:04:45 +0100

    new files

---
+++ programmering1_bte18/src/programmering1_bte18/grafikswing/grafikjtextfield.java
@@ -0,0 +1,53 @@
+package programmering1_bte18.grafikswing;
+
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class grafikjtextfield extends JFrame{
+
+	JTextField f1= new JTextField(10);
+	JTextField f2= new JTextField(10);
+	JLabel l1 = new JLabel();
+	JButton b1 = new JButton("beräkna");
+
+
+	public grafikjtextfield() {
+
+		setLayout(new FlowLayout());
+		add(f1);
+		add(f2);
+		add(b1);
+		add(l1);
+		setVisible(true);
+		setSize(new Dimension(400,400));
+
+		b1.addActionListener(e->{
+
+			int i1= Integer.parseInt(f1.getText());
+			int i2= Integer.parseInt(f2.getText());
+
+			int svar = i1*i2;
+
+			l1.setText(Integer.toString(svar));
+
+
+		});
+
+	}
+
+
+
+
+	public static void main(String[] args) {
+
+
+		new grafikjtextfield();
+	}
+
+}
---
+++ programmering1_bte18/src/programmering1_bte18/grafikswing/ÖkaMinska.java
@@ -0,0 +1,51 @@
+package programmering1_bte18.grafikswing;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ÖkaMinska extends JFrame {
+
+	int number = 0; // instansvariabel
+	JButton minska = new JButton("minska"); // skapar ett knapp objekt (minska) är inparameter till konstruktorn samt
+											// skapar referensvariabeln minska
+	JButton öka = new JButton("öka");
+	JLabel taltext = new JLabel("0"); //
+
+	public ÖkaMinska() { // konstruktor
+
+
+		setLayout(new FlowLayout()); // var saker skall placeras i frönstret och hur
+
+		minska.addActionListener(e -> {
+
+			int tal = --number;
+
+			taltext.setText(" " + tal);
+
+		});
+		öka.addActionListener(e -> {
+
+			int tal = ++number;
+
+			taltext.setText(" " + tal);
+
+		});
+
+		add(minska);
+		add(öka);
+		add(taltext);
+		setSize(200, 200);
+		this.setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+
+		new ÖkaMinska();
+
+	}
+
+}
