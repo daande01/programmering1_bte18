95d54929300ddb7d80742166b71295ed682e17f5
daande01 <daniel.evert.andersson@gmail.com>
2020-01-30 14:43:35 +0100

    boll2

--- programmering1_bte18/src/programmering1_bte18/egengrafik/boll1.java
+++ programmering1_bte18/src/programmering1_bte18/egengrafik/boll1.java
@@ -12,6 +12,7 @@ import javax.swing.Timer;
 public class boll1 extends JComponent {
 	int x = 100;
 	int y=100;
+	int xv=2;
 	int yv=3;
 	static Timer t;
 
@@ -28,11 +29,27 @@ public class boll1 extends JComponent {
 
 	public void update() {
 
-		x = x - 0;
+		yv=yv+2;
+
 		y=y+yv;
+		x=x+xv;
 		if (y>=300) {
 
 			yv=-yv;
+
+		}else if (y<=0) {
+
+			yv=yv*-1;
+
+		}
+
+		if (x>=300) {
+
+			xv=-xv;
+
+		}else if (x<=0) {
+
+			xv=xv*-1;
 
 		}
 
